f9978d857e7f7f28d09ee824332a5a5d85bfdd6b
Oghenemarho Orukele <morukele@yahoo.com>
2022-02-06 21:20:19 +0100

    clean up

---
+++ frontend/build/precache-manifest.8af0d1405249b9e02e14360f868e784a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048b002aa7c9de06eb3607f3de96c18d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6127c80ca70be2210995",
+    "url": "/static/js/2.5e01aa2c.chunk.js"
+  },
+  {
+    "revision": "76c76cd6136abb63b0f86286ed884d54",
+    "url": "/static/js/2.5e01aa2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9f6845a88b0d277cde",
+    "url": "/static/js/main.3c0eccf0.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
